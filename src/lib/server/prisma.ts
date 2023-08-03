b31a1537807328fde76a266f1597176c1ed98ad1
Maruchero <marce.gnz303@gmail.com>
2023-08-03 08:22:12 +0200

    Create activity (CRUD)

---
+++ src/lib/server/prisma.ts
@@ -0,0 +1,6 @@
+// $lib/server/prisma.ts
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+export default prisma
---
+++ src/routes/api/create-activity/+server.ts
@@ -0,0 +1,33 @@
+import prisma from '$lib/server/prisma';
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }): Promise<Response> => {
+    // Get data
+	const data = await request.formData();
+	const description = data.get('description') as string;
+	const completed = false; // By default when you create an activity it will be not completed.
+
+    // Check for errors
+	if (!description) throw error(400, "Missing parameter: 'description'.");
+
+	// Create the activity
+	const promise = new Promise<Response>((resolve, reject) => {
+		prisma.activity
+			.create({
+				data: {
+					description,
+					completed
+				}
+			})
+			.then((data) => {
+				console.log(data);
+				resolve(new Response(null, { status: 201 }));
+			})
+			.catch((err) => {
+				console.error(err);
+				reject(new Response('Unable to create activity. Try again', { status: 500 }));
+			});
+	});
+	return promise;
+};
